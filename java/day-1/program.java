d8a0c67b2c8b4c77972f804c596b8df620db4206
Atharv1136 <atharvbhosale00@gmail.com>
2026-06-22 13:40:05 +0530

    java First Program

---
+++ java/day-1/program.java
@@ -0,0 +1,14 @@
+class program {
+    public static void main(String[] args) {
+        System.out.println("Atharv bhosale");
+        System.out.println("atharvbhosale@gmail.com");
+        System.out.println(9561706020l);
+        System.out.println("pune");
+        System.out.println(
+                "Atharv Bhosale is a software developer based in Pune, India, passionate about building AI-powered applications, React frontends, and scalable full-stack systems. Atharv specializes in turning ideas into fast, accessible, and modern web products.\r\n"
+                        + //
+                        "\r\n" + //
+                        "BE Computer Science student passionate about solving real-world problems through innovation and teamwork. I’ve participated in multiple national hackathons and developed impactful solutions, including AI-powered crop advisory platforms and cybersecurity detection tools.");
+
+    }
+}
