39e1fd93287195d240a4c55b7a81380974a53034
Atharv1136 <atharvbhosale00@gmail.com>
2026-06-24 16:32:01 +0530

    java opretors

---
+++ java/day-2/addition.java
@@ -0,0 +1,205 @@
+class addition {
+	public static void main(String[] args) {
+		// --------addition----------------
+		byte b1 = 100;
+		byte b2 = 50;
+		int sub = b1 + b2;
+		System.out.println(sub);
+
+		short b3 = 100;
+		short b4 = 50;
+		int shot = b3 + b4;
+		System.out.println(shot);
+
+		int b5 = 50000;
+		int b6 = 60000;
+		int integer = b5 + b6;
+		System.out.println(integer);
+
+		char b7 = 'A';
+		char b8 = 'B';
+		int charResult = b7 + b8;
+		System.out.println(charResult);
+
+		float b9 = 1.1f;
+		float b10 = 2.2f;
+		float floatresult = b9 + b10;
+		System.out.println(floatresult);
+
+		double b11 = 1.1;
+		double b12 = 2.2;
+		double doubleresult = b11 + b12;
+		System.out.println(doubleresult);
+
+		String b13 = "Hello";
+		String b14 = "World";
+		String stringresult = b13 + (b14);
+		System.out.println(stringresult);
+
+		// ------------End of addition----------------
+		
+		// --------substraction----------------
+
+		byte b1 = 100;
+		byte b2 = 50;
+		int sub = b1 - b2;
+		System.out.println(sub);
+
+		short b3 = 100;
+		short b4 = 50;
+		int shot = b3 - b4;
+		System.out.println(shot);
+
+		int b5 = 50000;
+		int b6 = 60000;
+		int integer = b5 - b6;
+		System.out.println(integer);
+
+		char b7 = 'A';
+		char b8 = 'B';
+		int charResult = b7 - b8;
+		System.out.println(charResult);
+
+		float b9 = 1.1f;
+		float b10 = 2.2f;
+		float floatresult = b9 - b10;
+		System.out.println(floatresult);
+
+		double b11 = 1.1;
+		double b12 = 2.2;
+		double doubleresult = b11 - b12;
+		System.out.println(doubleresult);
+
+		String b13 = "Hello";
+		String b14 = "World";
+		String stringresult = b13 - (b14);
+		System.out.println(stringresult);
+
+		// ------------End of substraction----------------
+
+		// --------multiplication----------------
+
+		byte b1 = 100;
+		byte b2 = 50;
+		int sub = b1 * b2;
+		System.out.println(sub);
+
+		short b3 = 100;
+		short b4 = 50;
+		int shot = b3 * b4;
+		System.out.println(shot);
+
+		int b5 = 50000;
+		int b6 = 60000;
+		int integer = b5 * b6;
+		System.out.println(integer);
+
+		char b7 = 'A';
+		char b8 = 'B';
+		int charResult = b7 * b8;
+		System.out.println(charResult);
+
+		float b9 = 1.1f;
+		float b10 = 2.2f;
+		float floatresult = b9 * b10;
+		System.out.println(floatresult);
+
+		double b11 = 1.1;
+		double b12 = 2.2;
+		double doubleresult = b11 * b12;
+		System.out.println(doubleresult);
+
+		String b13 = "Hello";
+		String b14 = "World";
+		// String stringresult = b13 * (b14);
+		// System.out.println(stringresult);``
+		boolean b15 = true;
+		boolean b16 = false;
+		boolean booleanresult = b15 * (b16);
+		// ------------End of multiplication----------------
+
+		// --------division----------------
+
+		byte b1 = 100;
+		byte b2 = 50;
+		int sub = b1 / b2;
+		System.out.println(sub);
+
+		short b3 = 100;
+		short b4 = 50;
+		int shot = b3 / b4;
+		System.out.println(shot);
+
+		int b5 = 50000;
+		int b6 = 60000;
+		int integer = b5 / b6;
+		System.out.println(integer);
+
+		char b7 = 'A';
+		char b8 = 'B';
+		int charResult = b7 / b8;
+		System.out.println(charResult);
+
+		float b9 = 1.1f;
+		float b10 = 2.2f;
+		float floatresult = b9 / b10;
+		System.out.println(floatresult);
+
+		double b11 = 1.1;
+		double b12 = 2.2;
+		double doubleresult = b11 / b12;
+		System.out.println(doubleresult);
+
+		String b13 = "Hello";
+		String b14 = "World";
+		String stringresult = b13 / (b14);
+		System.out.println(stringresult);``
+
+		boolean b15 = true;
+		boolean b16 = false;
+		boolean booleanresult = b15 / (b16);
+		System.out.println(booleanresult);
+
+		// ------------End of division----------------
+
+		// --------modulus----------------
+
+		byte b1 = 100;
+		byte b2 = 50;
+		int sub = b1 % b2;
+		System.out.println(sub);
+
+		short b3 = 100;
+		short b4 = 50;
+		int shot = b3 % b4;
+		System.out.println(shot);
+
+		int b5 = 50000;
+		int b6 = 60000;
+		int integer = b5 % b6;
+		System.out.println(integer);
+
+		char b7 = 'A';
+		char b8 = 'B';
+		int charResult = b7 % b8;
+		System.out.println(charResult);
+
+		float b9 = 1.1f;
+		float b10 = 2.2f;
+		float floatresult = b9 % b10;
+		System.out.println(floatresult);
+
+		double b11 = 1.1;
+		double b12 = 2.2;
+		double doubleresult = b11 % b12;
+		System.out.println(doubleresult);
+
+		String b13 = "Hello";
+		String b14 = "World";
+		// String stringresult = b13 % (b14);
+		// System.out.println(stringresult);``
+
+		// ------------End of modulus----------------
+
+	}
+}
